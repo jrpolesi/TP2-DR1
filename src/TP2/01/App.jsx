d28f97bc6b7c4688b7384ebb8d8789b62d2a1828
José Ricardo Kremer Polesi <34324160+jrpolesi@users.noreply.github.com>
2024-08-30 03:01:03 +0000

    ex 1

--- src/TP2/01/App.jsx
+++ src/TP2/01/App.jsx
@@ -1,5 +1,30 @@
+/*
+Implemente um componente simples com React JS. Neste componente você deverá implementar: 
+
+Uma imagem qualquer; 
+Um título; 
+Um parágrafo.
+*/
+import "./app.css"
+import image from "./img/img.png";
+
+function Page() {
+  return (
+    <section>
+      <img src={image} alt="example" />
+
+      <h1>Lorem ipsum dolor sit amet</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi quia
+        eveniet corrupti odio nobis suscipit? Deleniti, distinctio corporis hic
+        dicta excepturi sed repellendus esse labore totam aliquid suscipit iure?
+        Dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perspiciatis laborum reprehenderit quibusdam voluptates expedita asperiores unde molestias voluptatibus, nobis eligendi illum illo! Quas id repudiandae voluptatem consectetur iste doloremque?
+      </p>
+    </section>
+  );
+}
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return <Page />;
 }
