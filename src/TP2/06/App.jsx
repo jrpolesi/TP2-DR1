6a0ece2ae07700c5130ba20c1b29def097be1e1d
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-08-31 15:29:47 -0300

    ex 6

--- src/TP2/06/App.jsx
+++ src/TP2/06/App.jsx
@@ -1,5 +1,41 @@
+/*
+Neste exercício, você deverá criar componentes isolados para construir um layout simples. O objetivo é criar os componentes Header, Footer e Card em React e montar o layout conforme o protótipo fornecido.
+*/
+import "./App.css";
+import Card from "./Card/Card";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+const CARDS_CONTENT = [
+  {
+    id: "Card 1",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+  {
+    id: "Card 2",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+];
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return (
+    <div className="page-layout">
+      <Header />
+
+      <div className="cards-list">
+        {CARDS_CONTENT.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 }
