704bd717c5cf7436b61689e6e0529d071c0ac0e4
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-08-31 14:52:03 -0300

    ex 5

--- src/TP2/05/App.jsx
+++ src/TP2/05/App.jsx
@@ -1,5 +1,49 @@
+/*
+Neste exercício, você deverá criar um componente isolado chamado Card.jsx contendo um título e um texto e fazer a chamada desse componente 4 vezes no componente principal..
+*/
+import "./App.css";
+import Card from "./Card/Card";
+
+const CARDS_CONTENT = [
+  {
+    id: "Card 1",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+  {
+    id: "Card 2",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+  {
+    id: "Card 3",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+  {
+    id: "Card 4",
+    title: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut sem fermentum ultricies. Nullam nec purus ut sem fermentum ultricies.",
+  },
+];
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return (
+    <div className="cards-page">
+      <h1>Cards</h1>
+      <div className="cards-list">
+        {CARDS_CONTENT.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/TP2/05/Card/Card.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function Card({ title, description }) {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
